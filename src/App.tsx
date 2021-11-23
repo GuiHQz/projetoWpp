febb22cb4e0e36aff5fe408d8b5bacb827152f83
Guilherme Queiroz <guilher.queiroz@gmail.com>
2021-11-23 17:04:20 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+//import "./App.styles.ts"
+import { Box } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //const style = useStyles();
+
+    return (
+        <Box className="app-window">
+            <Box className="sidebar">
+
+                <header>
+                    <img className="header--avatar" src="https://i.pinimg.com/736x/ad/55/73/ad5573610b7ff7ded5850ba931589cc3.jpg" alt="" />
+                    <Box className="header--buttons">
+                        <Box className="header--btn">
+                            <DonutLargeIcon style={{color: "#919191"}}/>
+                        </Box>
+                        <Box className="header--btn">
+                            <ChatIcon style={{color: "#919191"}}/>
+                        </Box>
+                        <Box className="header--btn">
+                            <MoreVertIcon style={{color: "#919191"}}/>
+                        </Box>
+                    </Box>
+                </header>
+
+                <Box className="search">
+                    search
+                </Box>
+
+                <Box className="chatlist">
+                    chatlist
+                </Box>
+
+            </Box>
+            <Box className="contentarea">
+                Essa é a contentarea
+            </Box>
+        </Box>
+    );
 }
 
 export default App;
